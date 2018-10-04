70f6075ddbf803c79831817e88d7f103a1a1e2ab
Валерий Сотиков <sotikov.valery@imperivm.team>
2018-10-04 22:54:11 +0500

    last progress

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  new SignForm ( { button: '.login-btn' } );
-  new LoginForm ( { button: '.reg-btn' } );
+  new SignForm ( { button: '.reg-btn' } );
+  new LoginForm ( { button: '.login-btn' } );
 });
 
 class AuthForm {
@@ -13,18 +13,32 @@ class AuthForm {
 
   showForm() {
     let wrapper = document.createElement('div');
+    wrapper.className = 'auth-form';
+
     let content = this.template();
     wrapper.innerHTML = content;
-    let form = document.body.querySelector('.content').appendChild(wrapper);
+
+    if ( this.otherFormOnPage() ) {
+      document.body.querySelector('.content')
+                   .removeChild(document.querySelector('.auth-form'));
+    }
+                   
+    if ( this.isOnPage() ) return;
+
+    if ( document.querySelector(this.selector) ) {
+      return;
+    }
+
+    let form = document.querySelector('.content').appendChild(wrapper);
 
     form.querySelector('.close-btn').addEventListener('click', () => {
-      document.body.querySelector('.content').removeChild(form);
+      document.querySelector('.content').removeChild(form);
     });
   }
 
-  // closeForm(form) {
-  //   document.body.removeChild(form);
-  // }
+  isOnPage() {
+    return document.querySelector('.auth-form');
+  }
 
   onClick(node) {
     node.addEventListener('click', this.showForm.bind(this) );
@@ -44,32 +58,12 @@ class SignForm extends AuthForm {
     super(obj);
   }
 
+  otherFormOnPage() {
+    return document.querySelector('.login-form');
+  }
+
   template() {
     return `<form action="">
-              <div id="login-form" class="login-form">
-                <div id="form-title" class="form-title">Authentication</div>
-                <img id="close-btn" class="close-btn"src="./close.png" alt="">
-                <label id="login-label" class="login-label" for="login-input">
-                  User login
-                </label>
-                <input id="login-input" class="login-input" type="text">
-                <label id="password-label" class="password-label" for="password-input">
-                  Password
-                </label>
-                <input id="password-input" class="password-input" type="password">
-                <div id="submit-login-btn" class="submit-login-btn" tabindex="1">Submit</div>
-              </div>
-            </form>`;
-  }
-};
-
-class LoginForm extends AuthForm {
-  constructor(obj) {
-    super(obj);
-  }
-
-  template() {
-    return  `<form action="">
               <div id="sign-form" class="sign-form">
                 <div id="form-title" class="form-title">Authorization</div>
                 <img id="close-btn" class="close-btn"src="./close.png" alt="">
@@ -86,6 +80,34 @@ class LoginForm extends AuthForm {
                 </label>
                 <input id="passconf-input" class="passconf-input" type="password">
               <div id="submit-auth-btn" class="submit-auth-btn" tabindex="1">Submit</div>
+              </div>
+            </form>`;
+  }
+};
+
+class LoginForm extends AuthForm {
+  constructor(obj) {
+    super(obj);
+  }
+
+  otherFormOnPage() {
+    return (document.querySelector('.sign-form'));
+  }
+
+  template() {
+    return  `<form action="">
+              <div id="login-form" class="login-form">
+                <div id="form-title" class="form-title">Authentication</div>
+                <img id="close-btn" class="close-btn"src="./close.png" alt="">
+                <label id="login-label" class="login-label" for="login-input">
+                  User login
+                </label>
+                <input id="login-input" class="login-input" type="text">
+                <label id="password-label" class="password-label" for="password-input">
+                  Password
+                </label>
+                <input id="password-input" class="password-input" type="password">
+                <div id="submit-login-btn" class="submit-login-btn" tabindex="1">Submit</div>
               </div>
             </form>`;
   }
