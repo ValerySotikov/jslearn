d3f34e305eb0018a4e6492b3030c658628c6dda6
Валерий Сотиков <sotikov.valery@imperivm.team>
2018-10-04 21:46:19 +0500

    last progress

--- index.js
+++ index.js
@@ -5,38 +5,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
 class AuthForm {
   constructor( { button } ) {
-    this.button = button;
+    this.button = this.findElement(button);
+    this.init();
+  }
+
+  submitForm() {}
+
+  showForm() {
+    let wrapper = document.createElement('div');
+    let content = this.template();
+    wrapper.innerHTML = content;
+    let form = document.body.querySelector('.content').appendChild(wrapper);
+
+    form.querySelector('.close-btn').addEventListener('click', () => {
+      document.body.querySelector('.content').removeChild(form);
+    });
+  }
+
+  // closeForm(form) {
+  //   document.body.removeChild(form);
+  // }
+
+  onClick(node) {
+    node.addEventListener('click', this.showForm.bind(this) );
+  }
+
+  findElement(elem) {
+    return document.querySelector(elem);
+  }
+
+  init() {
+    this.onClick(this.button);
   }
 };
 
 class SignForm extends AuthForm {
-  constructor() {}
-
-  template() {
-    return `<form action="">
-              <div id="sign-form" class="sign-form">
-                <div id="form-title" class="form-title">Authorization</div>
-                <img id="close-btn" class="close-btn"src="./close.png" alt="">
-                <label id="login-label" class="login-label" for="login-input">
-                  Enter login
-                </label>
-                <input id="login-input" class="login-input" type="text">
-                <label id="password-label" class="password-label" for="password-input">
-                  Enter <br> password
-                </label>
-                <input id="password-input" class="password-input" type="password">
-                <label id="passconf-label" class="passconf-label" for="passconf-input">
-                  Confirm password
-                </label>
-                <input id="passconf-input" class="passconf-input" type="password">
-              <div id="submit-auth-btn" class="submit-auth-btn" tabindex="1">Submit</div>
-              </div>
-            </form>`;
+  constructor(obj) {
+    super(obj);
   }
-};
-
-class LoginForm extends AuthForm {
-  constructor() {}
 
   template() {
     return `<form action="">
@@ -57,74 +63,30 @@ class LoginForm extends AuthForm {
   }
 };
 
+class LoginForm extends AuthForm {
+  constructor(obj) {
+    super(obj);
+  }
 
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   new RegWindow({ elem: '.reg-btn' });
-// });
-
-// class RegWindow {
-//   constructor({ elem }) {
-//     this.elem = this.findElement(elem);
-//     this.init();
-//   }
-
-//   onclick(node) {
-//     node.addEventListener('click', this.showForm.bind(this));
-//   }
-
-//   findElement(elem) {
-//     return document.querySelector(elem);
-//   }
-
-//   template() {
-//     return `
-//       <form id="reg-form" class="reg-form">
-//         <div id="input-field" class='input-field'>
-//           <label id="log-label" class="log-label" for="log-input">Login</label>
-//           <input id="log-input" class="log-input" type="text">
-//         </div>
-//         <div id="input-field" class='input-field'>
-//           <label id="pass-label" class="pass-label" for="pass-input">Password</label>
-//           <input class="pass-input" type="password">
-//         </div>
-//         <button type="button" id="submit-btn" class="submit-btn">Submit</button>
-//       </form>
-//     `;
-//   }
-
-
-
-
-
-
-
-//   showForm() {
-//     let wrapper = document.createElement('div');
-//     let content = this.template();
-//     wrapper.innerHTML = content;
-//     document.body.appendChild(wrapper);
-
-//     new Promise((resolve, reject) => {
-//       setInterval(() => {
-//         if (document.body.contains(wrapper)) resolve();
-//       }, 200);
-//     }).then(() => {
-//       this.elem.setAttribute('disabled', true);
-//       document.querySelector('.submit-btn').addEventListener('click', this.submitForm.bind(this));
-//     });
-//   }
-
-//   submitForm() {
-//     let login = document.querySelector('.log-input').value;
-//     let password = document.querySelector('.pass-input').value;
-    
-//     if (login === "Valery S" && password === "123456") console.log('You\'re submitted!');
-//     else console.log('Incorrect login or password!');
-//   }
-
-//   init() {
-//     this.onclick(this.elem);
-//   }
-// }
+  template() {
+    return  `<form action="">
+              <div id="sign-form" class="sign-form">
+                <div id="form-title" class="form-title">Authorization</div>
+                <img id="close-btn" class="close-btn"src="./close.png" alt="">
+                <label id="login-label" class="login-label" for="login-input">
+                  Enter login
+                </label>
+                <input id="login-input" class="login-input" type="text">
+                <label id="password-label" class="password-label" for="password-input">
+                  Enter <br> password
+                </label>
+                <input id="password-input" class="password-input" type="password">
+                <label id="passconf-label" class="passconf-label" for="passconf-input">
+                  Confirm password
+                </label>
+                <input id="passconf-input" class="passconf-input" type="password">
+              <div id="submit-auth-btn" class="submit-auth-btn" tabindex="1">Submit</div>
+              </div>
+            </form>`;
+  }
+};
