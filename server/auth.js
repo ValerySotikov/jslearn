7a0761813a49d255b8c74aaefcf1563dde75cb20
imperivm_trade_02 <valery.sotikov@imperivm.team>
2018-10-05 12:56:01 +0500

    last progress

--- server/auth.js
+++ server/auth.js
@@ -9,4 +9,12 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
 
+router.get('/authorize', (req, res) => {
+  console.log('Authorizing');
+});
+
+router.get('/authenticate', (req, res) => {
+  console.log('Authenticating');
+});
+
 module.exports = router;
